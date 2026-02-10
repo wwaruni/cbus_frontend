f889a73d758f2df2396953993a89bbdf10212bb5
Waruni Wickramanayake <wwaruni@gmail.com>
2026-02-10 14:41:42 +1100

    README Update

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Cbus Customer Dashboard Front End
 
 ## Prerequisits
-* Latest Node version
-* yarn or npm for package management
+* Latest Node version (v25.6.0)
+* yarn or npm for package management (yarn version v1.22.22)
   
 ##  Local Setup
 
 * checkout code repo
-* run ` yarn`
+* run ` yarn`. This will install all the node modules
 * `yarn dev`
 * Make sure to have a .env file based on env.sample and replace that with real values
 
--- src/components/CustomerListTable/index.tsx
+++ src/components/CustomerListTable/index.tsx
@@ -4,23 +4,42 @@ import Paper from "@mui/material/Paper";
 import type { GridPaginationModel } from "@mui/x-data-grid";
 
 const columns: GridColDef[] = [
-  { field: "customerId", headerName: "Customer ID", sortable: false, flex: 1, minWidth: 150 },
-  { field: "fullName", headerName: "Full Name", sortable: false, flex: 1, minWidth: 200 },
-  { field: "email", headerName: "Email", sortable: false, flex: 1, minWidth: 200 },
+  {
+    field: "customerId",
+    headerName: "Customer ID",
+    sortable: false,
+    flex: 1,
+    minWidth: 150,
+  },
+  {
+    field: "fullName",
+    headerName: "Full Name",
+    sortable: false,
+    flex: 1,
+    minWidth: 200,
+  },
+  {
+    field: "email",
+    headerName: "Email",
+    sortable: false,
+    flex: 1,
+    minWidth: 200,
+  },
   {
     field: "registrationDate",
     headerName: "Registration Date",
     sortable: false,
-    flex: 1, minWidth: 200
+    flex: 1,
+    minWidth: 200,
   },
 ];
 
 interface CustomerListProps {
-  customerDetails: CustomerDetails[],
-  setPaginationModel: any,
-  loading: boolean,
-  paginationModel: GridPaginationModel,
-  rowCount: number,
+  customerDetails: CustomerDetails[];
+  setPaginationModel: any;
+  loading: boolean;
+  paginationModel: GridPaginationModel;
+  rowCount: number;
 }
 /**
  * @param customerDetails - List of customers to be displayed
@@ -53,16 +72,20 @@ const CustomerListTable = ({
       ) : (
         <Paper sx={{ height: "100%", width: "100%" }}>
           <h2>Customer Dashboard</h2>
-          <DataGrid
-            rows={prepareRowData(customerDetails)}
-            columns={columns}
-            initialState={{ pagination: { paginationModel } }}
-            paginationMode="server"
-            paginationModel={paginationModel}
-            onPaginationModelChange={setPaginationModel}
-            rowCount={rowCount}
-            aria-label="Customer List"
-          />
+          {customerDetails ? (
+            <DataGrid
+              rows={prepareRowData(customerDetails)}
+              columns={columns}
+              initialState={{ pagination: { paginationModel } }}
+              paginationMode="server"
+              paginationModel={paginationModel}
+              onPaginationModelChange={setPaginationModel}
+              rowCount={rowCount}
+              aria-label="Customer List"
+            />
+          ) : (
+            "No Data"
+          )}
         </Paper>
       )}
     </>
